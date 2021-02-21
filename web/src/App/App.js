574979da82b1623c50b24284a231c6efd81d3acb
Oğuzhan Kiyar <oguzhan.kiyar@outlook.com>
2021-02-21 22:50:32 +0300

    Update app styles

--- web/src/App/App.js
+++ web/src/App/App.js
@@ -7,7 +7,7 @@ import './App.css';
 
 export default class App extends Component {
 
-    state = { isConnected: false, code: '', name: '', peers: [], files: [] };
+    state = { isConnected: false, code: '', name: '', isPeersShowing: true, isFilesShowing: false, peers: [], files: [] };
     inputFile = createRef();
 
     connector = null;
@@ -68,6 +68,14 @@ export default class App extends Component {
         this.connector.downloadFile(file);
     }
 
+    showPeers() {
+        this.setState({ ...this.state, isPeersShowing: true, isFilesShowing: false });
+    }
+    
+    showFiles() {
+        this.setState({ ...this.state, isPeersShowing: false, isFilesShowing: true });
+    }
+
     componentDidMount = () => {
         if (location.hash) {
             const hash = location.hash.substring(1).toUpperCase();
@@ -96,68 +104,83 @@ export default class App extends Component {
             <div className="App">
                 <Header />
                 <div className="container">
-                    <div style={{ float: 'left', marginLeft: 12.5, marginRight: 12.5 }}>
-                        <div dangerouslySetInnerHTML={{ __html: svg }} />
-                    </div>
-                    <div style={{ float: 'left', marginLeft: 12.5, marginRight: 12.5 }}>
-                        <div style={this.state.isConnected ? { display: 'block' } : { display: 'none' }}>
-                            <span className="label">Code</span>
-                            <input className="code" value={this.state.code} disabled />
-                            <span className="label">Name</span>
-                            <input className="name" value={this.state.name} disabled />
-                            <div>
-                                <input type="file" ref={this.inputFile} onChange={(event) => this.shareFile(event.target.files[0])} style={{ display: 'none' }} />
-                                <button type="submit" className="add" onClick={() => this.inputFile.current.click()}>Add File</button>
+                    <div className="left">
+                        <div className="qr">
+                            <div dangerouslySetInnerHTML={{ __html: svg }} />
+                        </div>
+                        <div className="form">
+                            <div style={this.state.isConnected ? { display: 'block' } : { display: 'none' }}>
+                                <span className="label">Code</span>
+                                <input className="code" value={this.state.code} disabled />
+                                <span className="label">Name</span>
+                                <input className="name" value={this.state.name} disabled />
+                                <div>
+                                    <input type="file" ref={this.inputFile} onChange={(event) => this.shareFile(event.target.files[0])} style={{ display: 'none' }} />
+                                    <button type="submit" className="add" onClick={() => this.inputFile.current.click()}>Add File</button>
+                                </div>
+                            </div>
+                            <div style={this.state.isConnected ? { display: 'none' } : { display: 'block' }}>
+                                <span className="label">Code</span>
+                                <input className="code" value={this.state.code} onChange={this.onCodeChange} onKeyPress={(event) => { if (event.key === 'Enter') this.startConnection(); }} autoComplete="none" maxLength="8" />
+                                <span className="label">Name</span>
+                                <input className="name" value={this.state.name} onChange={this.onNameChange} onKeyPress={(event) => { if (event.key === 'Enter') this.startConnection(); }} autoComplete="none" />
+                                <div>
+                                    <button type="submit" className="start" disabled={!this.state.code || !this.state.name} onClick={this.startConnection}>Start</button>
+                                </div>
                             </div>
                         </div>
-                        <div style={this.state.isConnected ? { display: 'none' } : { display: 'block' }}>
-                            <span className="label">Code</span>
-                            <input className="code" value={this.state.code} onChange={this.onCodeChange} onKeyPress={(event) => { if (event.key === 'Enter') this.startConnection(); }} autoComplete="none" maxLength="8" />
-                            <br />
-                            <span className="label">Name</span>
-                            <input className="name" value={this.state.name} onChange={this.onNameChange} onKeyPress={(event) => { if (event.key === 'Enter') this.startConnection(); }} autoComplete="none" />
-                            <div>
-                                <button type="submit" className="start" disabled={!this.state.code || !this.state.name} onClick={this.startConnection}>Start</button>
-                            </div>
-                        </div>
                     </div>
-                    <br />
-                    <br />
-                    <div style={{ display: 'table', clear: 'both', margin: 12.5, marginTop: 25 }}>
-                        {
-                            this.state.peers.length > 0 ? <div>Peers</div> : <></>
-                        }
-                        {
-                            this.state.peers.map((value, index) => {
-                                return (
-                                    <div className="peer" key={index}>
-                                        <div className="name">{value.name}</div>
-                                        <div className="info">at <div className="time">{
-                                            ("0" + new Date(value.time).getHours()).slice(-2) + ":" +
-                                            ("0" + new Date(value.time).getMinutes()).slice(-2) + ":" +
-                                            ("0" + new Date(value.time).getSeconds()).slice(-2)
-                                        }</div></div>
-                                    </div>
-                                )
-                            })
-                        }
-                        {
-                            this.state.files.length > 0 ? <div>Files</div> : <></>
-                        }
-                        {
-                            this.state.files.map((value, index) => {
-                                return (
-                                    <div className="file" key={index} onClick={() => this.onFileDownload(value)}>
-                                        <div className="name">{value.name}</div>
-                                        <div className="info">at <div className="time">{
-                                            ("0" + new Date(value.time).getHours()).slice(-2) + ":" +
-                                            ("0" + new Date(value.time).getMinutes()).slice(-2) + ":" +
-                                            ("0" + new Date(value.time).getSeconds()).slice(-2)
-                                        }</div> by <div className="owner">{value.owner.name}</div></div>
-                                    </div>
-                                )
-                            })
-                        }
+                    <div className="right">
+                        <div className="tabs">
+                            <div onClick={() => this.showPeers()} className={this.state.isPeersShowing ? 'tab active' : 'tab'} style={{ display: 'inline-table', marginRight: 10 }}>Peers{this.state.peers.length > 0 ? ' (' + this.state.peers.length + ')' : ''}</div>
+                            <div onClick={() => this.showFiles()} className={this.state.isFilesShowing ? 'tab active' : 'tab'} style={{ display: 'inline-table', marginRight: 10 }}>Files{this.state.files.length > 0 ? ' (' + this.state.files.length + ')' : ''}</div>
+                        </div>
+                        <div className="content">
+                            {
+                                this.state.isPeersShowing
+                                    ?
+                                        this.state.peers.length > 0
+                                        ?
+                                            this.state.peers.map((value, index) => {
+                                                return (
+                                                    <div className="peer" key={index}>
+                                                        <div className="name">{value.name}</div>
+                                                        <div className="info">at <div className="time">{
+                                                            ("0" + new Date(value.time).getHours()).slice(-2) + ":" +
+                                                            ("0" + new Date(value.time).getMinutes()).slice(-2) + ":" +
+                                                            ("0" + new Date(value.time).getSeconds()).slice(-2)
+                                                        }</div></div>
+                                                    </div>
+                                                )
+                                            })
+                                        :                                
+                                        <div className="warning">There is no peers to show</div>
+                                    :
+                                    <></>
+                            }
+                            {
+                                this.state.isFilesShowing
+                                ?
+                                    this.state.files.length > 0
+                                        ?
+                                        this.state.files.map((value, index) => {
+                                            return (
+                                                <div className="file" key={index} onClick={() => this.onFileDownload(value)}>
+                                                    <div className="name">{value.name}</div>
+                                                    <div className="info">at <div className="time">{
+                                                        ("0" + new Date(value.time).getHours()).slice(-2) + ":" +
+                                                        ("0" + new Date(value.time).getMinutes()).slice(-2) + ":" +
+                                                        ("0" + new Date(value.time).getSeconds()).slice(-2)
+                                                    }</div> by <div className="owner">{value.owner.name}</div></div>
+                                                </div>
+                                            )
+                                        })
+                                        :
+                                        <div className="warning">There is no files to show</div>
+                                :
+                                <></>
+                            }
+                        </div>
                     </div>
                 </div>
             </div>
