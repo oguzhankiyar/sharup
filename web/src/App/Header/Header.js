2df24a02f38cdafcd0d7ebfeb34af0b6b0ecad05
Oğuzhan Kiyar <oguzhan.kiyar@outlook.com>
2021-02-28 21:36:54 +0300

    Add sharup and github logo to header

--- web/src/App/Header/Header.js
+++ web/src/App/Header/Header.js
@@ -6,7 +6,15 @@ export class Header extends Component {
     render() {
         return (
             <div className="Header">
+                <span className="logo">
+                    <img src={process.env.PUBLIC_URL + '/img/sharup.svg'} alt="Logo" />
+                </span>
                 Sharup
+                <span className="github">
+                    <a href="https://github.com/oguzhankiyar/sharup" target="_blank">
+                        <img src={process.env.PUBLIC_URL + '/img/github.svg'} title="Show on GitHub" alt="GitHub" />
+                    </a>
+                </span>
             </div>
         );
     }
