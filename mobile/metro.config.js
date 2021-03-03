59a7234eb46f7f242eb3d633d7189ab40b58b65b
Oğuzhan Kiyar <oguzhan.kiyar@outlook.com>
2021-03-04 00:52:47 +0300

    Add svg support for qr and logo

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,5 +1,16 @@
-module.exports = {
-  transformer: {
-    assetPlugins: ['expo-asset/tools/hashAssetFiles'],
-  },
-};
+const { getDefaultConfig } = require('metro-config');
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts },
+  } = await getDefaultConfig();
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg'],
+    },
+  };
+})();
